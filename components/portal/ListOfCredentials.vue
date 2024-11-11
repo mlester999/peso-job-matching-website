ad71b15b64997ea95c56cef211237e16343ac636
mlester999 <mister6virgo@gmail.com>
2024-11-11 20:17:35 +0800

    updated list of credentials portal page

---
+++ components/portal/ListOfCredentials.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <OnboardingListOfCredentialsForm :isFromDashboard="true" :isCreate="true" />
+</template>
---
+++ pages/portal/list-of-credentials/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+definePageMeta({
+    layout: 'authenticated',
+});
+</script>
+
+<template>
+
+    <Head>
+        <Title>PESO Job Matching | List of Credentials</Title>
+        <Meta name="description" content="PESO Job Matching | List of Credentials" />
+    </Head>
+    <PortalListOfCredentials />
+</template>
